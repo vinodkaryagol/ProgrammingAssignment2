fe98ba6b9e3aafe69e1ee0d62831efe470bd74f6
vinodkaryagol <31212234+vinodkaryagol@users.noreply.github.com>
2018-03-28 11:12:08 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix function takes any sqaure matrix (invertible matrix)
+# as an argument and calculates inverse of square matrix using solve function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+  matrixInverseCopy<-NULL      # Initialized matrixInverseCopy with NULL
+  set<-function(y){
+    x <<- y                     # Assign value of matrix i.e. 'x'
+    matrixInverseCopy <<- NULL         
+  }
+  get <- function() x        # get value of matrix i.e. 'x'
+  
+  # setInverse function calcualtes inverse of matrix 'x' and assign the 
+  # result to matrixInverseCopy
+  setInverse <- function (solve) matrixInverseCopy <<- solve
+  # getInverse function is used to get inverse of matrix 'x'
+  getInverse <- function() matrixInverseCopy
+  
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
+# cacheSolve function takes matrix as an argument and checks whether matrix
+# inverse is calculated by makeCacheMatrix function. If it is calculated than
+# it print message by returning a inverse of matrix else calculate  
+# Inverse of matrix 'x' and return it
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x, ...){
+  matrixInverse <- x$getInverse()  # Retrieve matrix inverse 
+  if(!is.null(matrixInverse)){   # If matrix inverse is calculated
+    message("Getting cached data: Matrix inverse is already calculated")
+    return(matrixInverse) # Return a matrix that is the inverse of 'x'
+  }
+  data <- x$get()      #  Retrieve a matrix using get method
+  matrixInverse <- solve(data, ...)  # Calculate inverse of matrix
+  x$setInverse(matrixInverse) # Set value of inverse of matrix
+  (matrixInverse)  # Return a matrix that is the inverse of 'x'
 }
